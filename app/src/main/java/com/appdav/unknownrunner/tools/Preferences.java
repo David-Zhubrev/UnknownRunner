9bf753e5e5eeb8135c187e482181c4601603e944
David Zhubrev <david.zhubrev@gmail.com>
2020-10-13 13:12:07 +0300

    -Added highscore

---
+++ app/src/main/java/com/appdav/unknownrunner/tools/Preferences.java
@@ -0,0 +1,23 @@
+package com.appdav.unknownrunner.tools;
+
+import android.content.SharedPreferences;
+
+public class Preferences {
+
+    private static SharedPreferences mPreferences;
+    private static final String key = "highscore";
+    public static final String PREF_NAME = "pref";
+
+    public static void setupPreferences(SharedPreferences preferences){
+        mPreferences = preferences;
+    }
+
+    public static void writeHighScore(long highScore) {
+        mPreferences.edit().putLong(key, highScore).apply();
+    }
+
+    public static long getHighScore() {
+        return mPreferences.getLong(key, 0);
+    }
+
+}
